06df2460f3b7daae994384017122d4e7cd5b185f
GeronimoTiscornia <gero.tisco@gmail.com>
2024-05-14 10:09:30 -0300

    casi terminada el ejercicio2

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -50,4 +50,42 @@ auto.avanzar(15);
 console.log('La posición final del auto es: ' + auto.posicion);
 
 //Ejercicio 4//
+let nuevoAuto = {
+    marcA: "Volkswagen",
+    modelO: "Golf",
+    aniO: 2019,
+    colOr: "gris oscuro",
+    posiciOn: 0,
+    moverse: function(n) {
+        if (n => 0) {
+            this.posiciOn += n;
+        }
+        else {
+            this.posiciOn -= n;
+        }
+    }
+};
+nuevoAuto.moverse(10)
+nuevoAuto.moverse(-40)
+nuevoAuto.moverse(50)
 
+console.log("La posicion final del auto es: " + nuevoAuto.posiciOn);
+
+//Ejericio 5//
+let ironMan = {
+    Nombre: "Iron Man",
+    equipo: "Avengers",
+    poderes: ["Volar", "lanzar misiles", "Disparar lasers"],
+    energia: 100,
+    getPoder: function(y) {
+        if (y <= this.poderes.length) {
+         this.energia -= 10;
+         return "poder elegido de" + Nombre + ":" +  poderes + ". " +  "energia restante: " + energia + "."
+        } else {
+            return "¡El número de poder elegido es inválido!";
+        }
+    }
+};
+console.log(ironMan.getPoder(1)); // Volar
+console.log(ironMan.getPoder(2)); // Lanzar misiles
+console.log(ironMan.getPoder(3)); // Disparar láser
